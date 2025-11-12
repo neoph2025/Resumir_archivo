ca8a6e4a1965485e6226e050b8917ce4c755d271
neoph2025 <rino_ph@icloud.com>
2025-11-12 19:27:27 +0100

    Update main.py
    Version del BC

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import openai
 import streamlit as st
 from langchain import PromptTemplate
 from langchain_openai import OpenAI
@@ -7,20 +6,19 @@ from langchain.text_splitter import RecursiveCharacterTextSplitter
 import pandas as pd
 from io import StringIO
 
-
-# LLM and key loading function
-def load_LLM():
+#LLM and key loading function
+def load_LLM(openai_api_key):
     # Make sure your openai_api_key is set as an environment variable
-
-    llm = OpenAI(model="gpt-3.5-turbo", api_key=openai_api_key)
+    llm = OpenAI(temperature=0, openai_api_key=openai_api_key)
     return llm
 
 
-# Page title and header
-st.set_page_config(page_title="AI - Resumen de texto")
-st.header("AI - Resumen de texto")
+#Page title and header
+st.set_page_config(page_title="AI Long Text Summarizer")
+st.header("AI Long Text Summarizer")
 
-# Intro: instructions
+
+#Intro: instructions
 col1, col2 = st.columns(2)
 
 with col1:
@@ -29,32 +27,42 @@ with col1:
 with col2:
     st.write("Contact with [AI Accelera](https://aiaccelera.com) to build your AI Projects")
 
-openai_api_key = st.secrets["OPENAI_API_KEY"]
+
+#Input OpenAI API Key
+st.markdown("## Enter Your OpenAI API Key")
+
+def get_openai_api_key():
+    input_text = st.text_input(label="OpenAI API Key ",  placeholder="Ex: sk-2twmA8tfCb8un4...", key="openai_api_key_input", type="password")
+    return input_text
+
+openai_api_key = get_openai_api_key()
+
 
 # Input
 st.markdown("## Upload the text file you want to summarize")
 
 uploaded_file = st.file_uploader("Choose a file", type="txt")
 
+       
 # Output
 st.markdown("### Here is your Summary:")
 
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    # st.write(bytes_data)
+    #st.write(bytes_data)
 
     # To convert to a string based IO:
     stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    # st.write(stringio)
+    #st.write(stringio)
 
     # To read file as string:
     string_data = stringio.read()
-    # st.write(string_data)
+    #st.write(string_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    # dataframe = pd.read_csv(uploaded_file)
-    # st.write(dataframe)
+    #dataframe = pd.read_csv(uploaded_file)
+    #st.write(dataframe)
 
     file_input = string_data
 
@@ -65,24 +73,24 @@ if uploaded_file is not None:
     if file_input:
         if not openai_api_key:
             st.warning('Please insert OpenAI API Key. \
-            Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)',
-                       icon="⚠️")
+            Instructions [here](https://help.openai.com/en/articles/4936850-where-do-i-find-my-secret-api-key)', 
+            icon="⚠️")
             st.stop()
 
     text_splitter = RecursiveCharacterTextSplitter(
-        separators=["\n\n", "\n"],
-        chunk_size=5000,
+        separators=["\n\n", "\n"], 
+        chunk_size=5000, 
         chunk_overlap=350
-    )
+        )
 
     splitted_documents = text_splitter.create_documents([file_input])
 
-    llm = load_LLM()
+    llm = load_LLM(openai_api_key=openai_api_key)
 
     summarize_chain = load_summarize_chain(
-        llm=llm,
+        llm=llm, 
         chain_type="map_reduce"
-    )
+        )
 
     summary_output = summarize_chain.run(splitted_documents)
 
